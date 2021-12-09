130d2f7e864113b79900d0fef44ad4d28def85c5
khoa <khoa-dev203@outlook.com>
2021-12-10 01:27:04 +0300

    done task II

--- DOTPrettyPrintListener.java
+++ DOTPrettyPrintListener.java
@@ -1,15 +1,13 @@
-// Generated from DOT.g4 by ANTLR 4.7.2
 
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
-
 /**
  * This class provides an empty implementation of {@link DOTListener},
  * which can be extended to create a listener which only needs to handle a subset
  * of the available methods.
  */
-public class DOTBaseListener implements DOTListener {
+public class DOTPrettyPrintListener extends DOTBaseListener {
 	/**
 	 * {@inheritDoc}
 	 *
--- tmp/DOTPrettyPrintListener.java
+++
@@ -1,3 +0,0 @@
-public class DOTPrettyPrintListener implements DOTBaseListener {
-    
-}
